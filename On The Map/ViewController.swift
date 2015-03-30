9ef5ff4cb10516c5fb038d327c1dc6c869fae742
Nate Allison <nate.allison@gmail.com>
2015-03-30 08:13:35 -0600

    init

---
+++ On The Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  On The Map
+//
+//  Created by Nathan Allison on 3/30/15.
+//  Copyright (c) 2015 shondicon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
