4025f5c552536d4fc77f21b6ae4d1d4fef341d4b
Nate Allison <nate.allison@gmail.com>
2015-04-08 09:25:55 -0600

    play around with map, add playground

---
+++ On The Map/MapViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MapViewController.swift
+//  On The Map
+//
+//  Created by Nathan Allison on 3/30/15.
+//  Copyright (c) 2015 shondicon. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
